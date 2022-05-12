e4376ca19220400cb45b18d9cae06981b2eaf336
Rafael Viveiros <rafavbarbosa@gmail.com>
2022-05-12 10:25:47 -0300

    teste 5

--- src/code/index.js
+++ src/code/index.js
@@ -1,29 +1,21 @@
 
-const valueInput = document.getElementById('choice-value');
-const increament = document.getElementById('increament');
-const decreament = document.getElementById('decreament');
 const secretNumber = document.getElementById('secret-number-id');
 const playGameButton = document.getElementById('play-game-id');
 const reloadGameButton = document.getElementById('reload-game-id');
 const result = document.getElementById('result');
-const boxChangeNumber = document.querySelector('.changer-number-box');
+const rangeValue = document.getElementById('range-value');
+const choiceValueRange = document.getElementById('choice-value-range');
+
 
 const randomNumberGenerate = () => {
     let randomNumber = Math.floor(Math.random() * 11);
     return randomNumber
 };
 
-increament.addEventListener('click', () => {
-    if (valueInput.value < 10) {
-        valueInput.value =+ valueInput.value + 1;
-    } else {
-    }
-});
+rangeValue.innerHTML = choiceValueRange.value;
 
-decreament.addEventListener('click', () => {
-    if (valueInput.value > 0) {
-        valueInput.value =+ valueInput.value - 1;
-    }
+choiceValueRange.addEventListener("input", () => {
+    rangeValue.innerHTML = choiceValueRange.value;
 });
 
 
@@ -33,27 +25,23 @@ playGameButton.addEventListener('click', () => {
     let randomNumber = randomNumberGenerate();
 
     console.log(randomNumber)
-    if (valueInput.value == randomNumber) {
+    if (choiceValueRange.value == randomNumber) {
         secretNumber.innerHTML = randomNumber;
         result.innerHTML = "Acertou";
-        result.style.color = "green";
+        result.style.color = "#00ff00";
     } else {
         secretNumber.innerHTML = randomNumber;
-        valueInput.value = 0;
         result.style.color = "red";
         result.innerHTML = "Errou";
         playGameButton.style.display = "none";
         reloadGameButton.style.display = "block";
-        boxChangeNumber.style.display = "none";
     }
 });
 
 reloadGameButton.addEventListener('click', () => {
     playGameButton.style.display = "block";
     reloadGameButton.style.display = "none";
-    valueInput.value = 0;
     result.innerHTML = "Resultado";
     result.style.color = "black"
     secretNumber.innerHTML = "?";
-    boxChangeNumber.style.display = "flex";
 })
